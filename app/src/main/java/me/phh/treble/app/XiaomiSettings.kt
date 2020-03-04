1535ef8e908927a57ad86227df7e2250180e8d95
Victor Bo <bvoid@yandex.ru>
2020-03-04 13:24:36 +0200

    rework Xiaomi features
    	modified:   app/src/main/java/me/phh/treble/app/Xiaomi.kt
    	modified:   app/src/main/java/me/phh/treble/app/XiaomiSettings.kt
    	modified:   app/src/main/res/xml/pref_xiaomi.xml

--- app/src/main/java/me/phh/treble/app/XiaomiSettings.kt
+++ app/src/main/java/me/phh/treble/app/XiaomiSettings.kt
@@ -1,12 +1,18 @@
 package me.phh.treble.app
 
+import android.os.Bundle
 
 object XiaomiSettings : Settings {
-    val dt2w = "xiaomi_double_tap_to_wake"
+    val dt2w = "key_xiaomi_double_tap_to_wake"
 
     override fun enabled() = Tools.vendorFp.toLowerCase().startsWith("xiaomi")
 }
 
 class XiaomiSettingsFragment : SettingsFragment() {
     override val preferencesResId = R.xml.pref_xiaomi
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        super.onCreatePreferences(savedInstanceState, rootKey)
+        android.util.Log.d("PHH", "Loading xiaomi fragment ${XiaomiSettings.enabled()}")
+    }
 }
