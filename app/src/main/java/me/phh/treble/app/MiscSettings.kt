b3aa860aa099ca47b89a24f63e31df7e09d0af06
Victor Bo <bvoid@yandex.ru>
2020-10-16 08:05:14 -0400

    add enhanced root solution

--- app/src/main/java/me/phh/treble/app/MiscSettings.kt
+++ app/src/main/java/me/phh/treble/app/MiscSettings.kt
@@ -24,6 +24,7 @@ object MiscSettings : Settings {
     val navbarInverse = "key_misc_navbar_inverse"
     val bluetooth = "key_misc_bluetooth"
     val securize = "key_misc_securize"
+    val superUser = "key_misc_superuser"
     val removeTelephony = "key_misc_removetelephony"
     val remotectl = "key_misc_remotectl"
     val disableAudioEffects = "key_misc_disable_audio_effects"
